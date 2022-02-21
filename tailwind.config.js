02129cb74deae28d9616d71837932bcd1faa20e5
Cecilie Norrbohm <ceci332s@stud.kea.dk>
2022-02-21 13:46:03 +0100

    colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+
+    colors: {
+      blue: "#183599",
+      orange: "#FBB539",
+      dark: "#FBB539",
+      light: "#F5F8FF",
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
