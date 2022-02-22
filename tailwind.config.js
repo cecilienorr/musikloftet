e4676fe805f76a22bb08e7c98e55bfae2bd74d85
Cecilie Norrbohm <ceci332s@stud.kea.dk>
2022-02-22 09:30:31 +0100

    damage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      blue: "#183599",
-      orange: "#FBB539",
-      dark: "#000929",
-      light: "#F5F8FF",
-    },
-
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
-};
-module.exports = {
-  //...
-  plugins: [require("daisyui")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("daisyui"),
+  ],
 };
